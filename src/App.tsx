fd80a56d28ea13e5b5fd554dc9cc1af479a4fbf3
zhankaam <zhankaam@gmail.com>
2021-06-07 13:25:50 +0300

    :)

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,14 @@ import axios from "axios";
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-           padding:theme.spacing(10)
+            padding: theme.spacing(10)
         },
         paper: {
             padding: theme.spacing(2),
             textAlign: 'center',
             color: theme.palette.text.secondary
         },
-        cryptoInputBox:{
+        cryptoInputBox: {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
@@ -65,99 +65,101 @@ type CoinsType = {
 
 function App() {
     const classes = useStyles();
-    const [allCoins,setAllCoins] = useState<CoinsType[]>([]);
+    const [allCoins, setAllCoins] = useState<CoinsType[]>([]);
 
-    useEffect( () => {
-        async function getRequestData(){
+    useEffect(() => {
+        async function getRequestData() {
             let {data} = await axios.get(`https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD`)
-           const coins:CoinsType[] = data.Data.map((coin: any) => {
-             const obj: CoinsType = {
-                 name: coin.CoinInfo.Name,
-                 fullName: coin.CoinInfo.FullName,
-                 imageUrl: `https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
-                 price: coin.RAW.USD.PRICE.toFixed(2),
-                 volume24Hour: parseInt(coin.RAW.USD.VOLUME24HOUR),
-             }
-             return obj
-           })
+            const coins: CoinsType[] = data.Data.map((coin: any) => {
+                const obj: CoinsType = {
+                    name: coin.CoinInfo.Name,
+                    fullName: coin.CoinInfo.FullName,
+                    imageUrl: `https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
+                    price: coin.RAW.USD.PRICE.toFixed(2),
+                    volume24Hour: parseInt(coin.RAW.USD.VOLUME24HOUR),
+                }
+                return obj
+            })
             setAllCoins(coins)
         }
 
 
         getRequestData()
-    },[])
+    }, [])
 
-  return (
-  <Container maxWidth="lg" className={classes.root}>
-      <Grid container spacing={3}>
-          <Grid item xs={8}>
-                  <TableContainer component={Paper}>
-                      <Table className={classes.table} aria-label="simple table">
-                          <TableHead>
-                              <TableRow>
-                                  <TableCell></TableCell>
-                                  <TableCell align="left"><b>Name</b></TableCell>
-                                  <TableCell align="left"><b>FullName</b></TableCell>
-                                  <TableCell align="left"><b>Price</b></TableCell>
-                                  <TableCell align="left"><b>Volume24hour</b></TableCell>
-                              </TableRow>
-                          </TableHead>
-                          <TableBody>
-                              {allCoins?.map((coin) => (
-                                  <TableRow key={coin.name}>
-                                      <TableCell component="th" scope="row">
-                                          <img className={classes.icon} src={coin.imageUrl} alt={"try again later"}/>
-                                      </TableCell>
-                                      <TableCell align="left">{coin.name}</TableCell>
-                                      <TableCell align="left">{coin.fullName}</TableCell>
-                                      <TableCell align="left">${coin.price}</TableCell>
-                                      <TableCell align="left">${coin.volume24Hour}</TableCell>
-                                  </TableRow>
-                              ))}
-                          </TableBody>
-                      </Table>
-                  </TableContainer>
-          </Grid>
-          <Grid item xs={4}>
-              <Paper className={classes.paper}>
-                  <div className={classes.cryptoInputBox}>
-                      <FormControl className={classes.currencyInput} >
-                          <TextField label="Сумма" />
-                      </FormControl>
-                      <FormControl  className={classes.currencyType} >
-                          <InputLabel shrink id="demo-simple-select-placeholder-label-label">
-                              Валюта
-                          </InputLabel>
-                          <Select value={10} >
-                              <MenuItem value={10}>Ten</MenuItem>
-                              <MenuItem value={20}>Twenty</MenuItem>
-                              <MenuItem value={30}>Thirty</MenuItem>
-                          </Select>
-                      </FormControl>
-                  </div>
-                  <div className={classes.cryptoInputBox}>
-                      <FormControl className={classes.currencyInput} >
-                          <TextField label="Сумма" />
-                      </FormControl>
-                      <FormControl  className={classes.currencyType} >
-                          <InputLabel shrink id="demo-simple-select-placeholder-label-label">
-                              Валюта
-                          </InputLabel>
-                          <Select value={10} >
-                              <MenuItem value={10}>Ten</MenuItem>
-                              <MenuItem value={20}>Twenty</MenuItem>
-                              <MenuItem value={30}>Thirty</MenuItem>
-                          </Select>
-                      </FormControl>
-                  </div>
-                  <Typography variant={"h5"} component={"h5"}>
-                  77,81 Российский рубль
-                  </Typography>
-              </Paper>
-          </Grid>
-      </Grid>
-  </Container>
-  );
+    return (
+        <Container maxWidth="lg" className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={8}>
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell align="left"><b>Name</b></TableCell>
+                                    <TableCell align="left"><b>FullName</b></TableCell>
+                                    <TableCell align="left"><b>Price</b></TableCell>
+                                    <TableCell align="left"><b>Volume24hour</b></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {!allCoins.length
+                                    ? 'Loading...'
+                                    : allCoins?.map((coin) => (
+                                        <TableRow key={coin.name}>
+                                            <TableCell component="th" scope="row">
+                                                <img className={classes.icon} src={coin.imageUrl} alt={"try again later"}/>
+                                            </TableCell>
+                                            <TableCell align="left">{coin.name}</TableCell>
+                                            <TableCell align="left">{coin.fullName}</TableCell>
+                                            <TableCell align="left">${coin.price}</TableCell>
+                                            <TableCell align="left">${coin.volume24Hour}</TableCell>
+                                        </TableRow>
+                                    ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>
+                        <div className={classes.cryptoInputBox}>
+                            <FormControl className={classes.currencyInput}>
+                                <TextField label="Сумма"/>
+                            </FormControl>
+                            <FormControl className={classes.currencyType}>
+                                <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+                                    Валюта
+                                </InputLabel>
+                                <Select value={10}>
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
+                        <div className={classes.cryptoInputBox}>
+                            <FormControl className={classes.currencyInput}>
+                                <TextField label="Сумма"/>
+                            </FormControl>
+                            <FormControl className={classes.currencyType}>
+                                <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+                                    Валюта
+                                </InputLabel>
+                                <Select value={10}>
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
+                        <Typography variant={"h5"} component={"h5"}>
+                            77,81 Российский рубль
+                        </Typography>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default App;
